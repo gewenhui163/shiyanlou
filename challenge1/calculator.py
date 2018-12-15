8b8180d6228ffd413050738133053c253d371625
gewenhui163 <gewenhui123456@163.com>
2018-12-15 10:21:49 +0800

    modify calculator.py

--- challenge1/calculator.py
+++ challenge1/calculator.py
@@ -1,1 +1,29 @@
-print('ok')
+#!/usr/bin/env python3
+
+import sys
+
+salary = sys.argv[1]
+
+try:
+	money = int(salary)
+	over_money = money - 3500
+except:
+	print("Parameters Error")
+
+if over_money <= 0:
+	cal = 0
+elif over_money <= 1500:
+	cal = over_money * 0.03
+elif over_money <= 4500:
+	cal = over_money * 0.1 - 105
+elif over_money <= 9000:
+	cal = over_money * 0.2 - 555
+elif over_money <= 35000:
+	cal = over_money * 0.25 - 1005
+elif over_money <= 50000:
+	cal = over_money * 0.3 -2775
+elif over_money <= 80000:
+	cal = over_money * 0.35 - 5505
+else:
+	cal = over_money * 0.45 - 13505
+print("{:.2f}".format(cal))	
